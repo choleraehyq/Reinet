1409bfab494670c9f879813e19ea7fa0f20bd3cd
Cholerae Hu <choleraehyq@gmail.com>
2015-05-03 22:30:48 +0800

    Mod: refactor

--- reinet.go
+++ reinet.go
@@ -1,13 +1,8 @@
 package reinet
 
 import (
-	"strconv"
-	"strings"
-	"log"
-	"reflect"
 	"regexp"
 	"net/http"
-	"os"
 )
 
 const (
@@ -45,150 +40,14 @@ type route struct {
 	method string
 }
 
-type ReiServer struct {
-	routes []route
-	logger *log.Logger
-	staticDir *map[string]string
-}
-
 var mainServer *ReiServer
 var Sessions *Manager
 
 func init() {
 	mainServer = NewServer()
+	Sessions = nil
 	initSession()
 	Sessions, _ = NewManager("default", "reinetSessionID", 3600)
-	go Sessions.GC()
-}
-
-func NewServer() *ReiServer {
-	m := make(map[string]string)
-	return &ReiServer {
-		logger: log.New(os.Stdout, "", log.Ldate|log.Ltime),
-		staticDir: &m,
-	}
-}
-
-func (self *ReiServer) addRoute(pattern string, handleFunc handler, method string) {
-	parts := strings.Split(pattern, "/")
-	j := 0
-	params := make(map[int]string)
-	for i, part := range parts {
-		if strings.HasPrefix(part, ":") {
-			expr := "([^/]+)"
-			if index := strings.Index(part, "("); index != -1 {
-				expr = part[index:]
-				part = part[:index]
-			}
-			params[j] = part
-			parts[i] = expr
-			j++
-		}
-	}
-	
-	pattern = strings.Join(parts, "/")
-	regex, regexErr := regexp.Compile(pattern)
-	if regexErr != nil {
-		panic(regexErr)
-		return
-	}
-	
-	newRoute := route {
-		regex: regex, 
-		params: params,
-		handler: handleFunc,
-		method: method,
-	}
-	self.routes = append(self.routes, newRoute)
-}
-
-func requireContext(handlerType reflect.Type) bool {
-	if handlerType.NumIn() == 0 {
-		return false
-	}
-	if firstParam := handlerType.In(0); firstParam.Kind() != reflect.Ptr || firstParam.Elem() != reflect.TypeOf(Context{}) {
-		return false
-	}
-	return true
-}
-
-func (self *ReiServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	var started bool
-	requestPath := r.URL.Path
-	for prefix, staticDir := range *(self.staticDir) {
-		if strings.HasPrefix(requestPath, prefix) {
-			file := staticDir + requestPath[len(prefix):]
-			http.ServeFile(w, r, file)
-			started = true
-			return
-		}	
-	}
-	
-	for _, route := range self.routes {
-		if !route.regex.MatchString(requestPath) && route.method != r.Method {
-			continue
-		}
-		
-		matches := route.regex.FindStringSubmatch(requestPath)
-		
-		if len(matches[0]) != len(requestPath) {
-			continue
-		}
-		
-		r.ParseForm()
-		formParams := make(map[string]string)
-		if len(r.Form) > 0 {
-			for k, v := range r.Form {
-				formParams[k] = v[0]
-			}
-		}
-		
-		values := r.URL.Query()
-		urlQueryParams := make(map[string]string)
-		if len(values) > 0 {
-			for k, v := range values {
-				urlQueryParams[k] = v[0]
-			}
-		}
-		
-		params := make([]reflect.Value, 0)
-		
-		if requireContext(reflect.TypeOf(route.handler)) {
-			ctx := &Context {
-				req: r,
-				res: w,
-				formParams: formParams,
-				urlQueryParams: urlQueryParams,
-			}
-			params = append(params, reflect.ValueOf(ctx))
-		}
-		
-		for _, match := range matches[1:] {
-			params = append(params, reflect.ValueOf(match))
-		}
-		
-		rets := reflect.ValueOf(route.handler).Call(params)
-		var content []byte
-		if len(rets) != 0 {
-			sval := rets[0]
-			if sval.Kind() == reflect.String {
-				content = []byte(sval.String())
-			} else if sval.Kind() == reflect.Slice && sval.Type().Elem().Kind() == reflect.Uint8 {
-				content = sval.Interface().([]byte)
-			}
-			w.Header().Set("Content-Type", strconv.Itoa(len(content)))
-			_, err := w.Write(content)
-			if err != nil {
-				self.logger.Printf("Write content to client error: %v", err)
-			}
-		}
-		started = true
-		break
-	}
-	
-	if started == false {
-		http.NotFound(w, r)
-	}
 }
 
 func wrap(handleFunc handler) handler { 
@@ -224,5 +83,15 @@ func SetStatic(url string, path string) {
 }
 
 func Run(addr string) {
+	go Sessions.GC()
 	http.ListenAndServe(addr, mainServer)
+}
+
+func UseProvider(providerName string, provider Provider) {
+	AddProvider(providerName, provider)
+	Sessions.provider = provides[providerName]
+}
+
+func SetExpires(expires int64) {
+	Sessions.maxLifeTime = expires
 }
